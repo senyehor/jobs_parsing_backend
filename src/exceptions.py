053c27eee50271b250eee26b277d89b8380f2de9
Yehor Sencha <senyehor@gmail.com>
2025-01-20 15:32:13 +0200

    feat: make exception with message base

---
+++ src/exceptions.py
@@ -0,0 +1,10 @@
+class ExceptionWithMessageBase(Exception):
+    message: str = None
+    message_for_user: str = "An unexpected error occurred"
+
+    def __init__(self, message=None, message_for_user=None):
+        super().__init__(self.message or message)
+        self.message_for_user = self.message_for_user or message_for_user
+
+    def __str__(self):
+        return f"{super().__str__()}\n(User Message: {self.message_for_user})"
