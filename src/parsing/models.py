22b8b78ab81357ae048f6996e282dbd5d6027b6d
Yehor Sencha <senyehor@gmail.com>
2025-01-20 17:20:49 +0200

    fix: add None default value to optional fields

--- src/parsing/models.py
+++ src/parsing/models.py
@@ -1,19 +1,12 @@
 from datetime import datetime
-from enum import StrEnum
 
 from pydantic import BaseModel, HttpUrl
-
-
-class _EmploymentOptions(StrEnum):
-    remote = 'remote'
-    on_site = 'on-site'
-    hybrid = 'hybrid'
 
 
 class JobPosting(BaseModel):
     link: HttpUrl
     job_title: str
     company_name: str
-    location: str | None
-    employment_type: _EmploymentOptions
-    posted_at: datetime | None
+    location: str | None = None
+    employment_type: str
+    posted_at: datetime | None = None
