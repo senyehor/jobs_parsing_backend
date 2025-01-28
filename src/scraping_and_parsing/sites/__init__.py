f048a6b98fa5163edbb1d35b7c6e1ccd039f0798
Yehor Sencha <senyehor@gmail.com>
2025-01-28 16:34:35 +0200

    ref: add SITES_MAP_BY_NAME

--- src/scraping_and_parsing/sites/__init__.py
+++ src/scraping_and_parsing/sites/__init__.py
@@ -1,7 +1,12 @@
 from src.scraping_and_parsing.sites.dou import Dou
 from src.scraping_and_parsing.sites.genesis import Genesis
+from src.scraping_and_parsing.sites.site_base import SiteBase
 
-SITES = [
+SITES_LIST: list[type[SiteBase]] = [
     Dou,
     Genesis
 ]
+
+SITES_MAP_BY_NAME = {
+    site.site_name: site for site in SITES_LIST
+}
