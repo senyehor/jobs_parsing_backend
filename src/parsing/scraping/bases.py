29b130f082a0ad9bd55b0c966afe983f7869fd7a
Yehor Sencha <senyehor@gmail.com>
2025-01-28 15:56:14 +0200

    ref: add default httpx scraping behaviour

--- src/parsing/scraping/bases.py
+++ src/parsing/scraping/bases.py
@@ -2,8 +2,11 @@ from abc import ABC, abstractmethod
 from copy import copy
 from typing import Iterable
 
-from httpx import Client
+from httpx import AsyncClient, HTTPStatusError
 from selenium.webdriver.chrome.webdriver import WebDriver
+from urllib3.exceptions import RequestError
+
+from src.exceptions import ExceptionWithMessageForUser
 
 
 class HtmlScraperBase(ABC):
@@ -16,10 +19,20 @@ class HtmlScraperBase(ABC):
         pass
 
 
-class HttpxScraperBase(HtmlScraperBase, ABC):
-    def __init__(self, client: Client, url: str, keywords: list[str]):
+class HttpxScraperBase(HtmlScraperBase):
+    def __init__(self, client: AsyncClient, url: str, keywords: list[str]):
         super().__init__(url, keywords)
         self._client = client
+
+    async def scrape(self) -> str | Iterable[str]:
+        try:
+            response = await self._client.get(self._url)
+            response.raise_for_status()
+            return response.text
+        except (RequestError, HTTPStatusError) as e:
+            raise ExceptionWithMessageForUser(
+                message_for_user=f'Failed to fetch data from url: "f{self._url}"'
+            ) from e
 
 
 class SeleniumScraperBase(HtmlScraperBase, ABC):
