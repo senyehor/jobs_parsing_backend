bc2d4ddd8d5f7fc87b82909736c296e74ee488d9
Yehor Sencha <senyehor@gmail.com>
2025-01-30 13:57:45 +0200

    feat: add endpoint to fetch from all sites

--- src/main.py
+++ src/main.py
@@ -8,6 +8,7 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
 from src.scraping_and_parsing.models import JobPosting
+from src.scraping_and_parsing.sites import HTTPX_BASED_SITES, SELENIUM_BASED_SITES
 from src.scraping_and_parsing.sites.dou import DouParser, DouSeleniumScraper
 from src.scraping_and_parsing.sites.genesis import GenesisParser, GenesisScraper
 
@@ -25,6 +26,33 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+
+@app.get('/api/all-sites/')
+async def all_sites(keywords: Annotated[list[str], Query()]) -> Iterable[JobPosting]:
+    jobs = []
+    for site in SELENIUM_BASED_SITES:
+        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+        scraper = site.scraper_class(driver=driver)
+        try:
+            htmls = await scraper.scrape(site.base_url, keywords)
+            if isinstance(htmls, str):
+                htmls = (htmls,)
+            for html in htmls:
+                parser = site.parser_class(html)
+                jobs.extend(parser.parse_jobs(keywords))
+        finally:
+            driver.quit()
+    async with AsyncClient() as client:
+        for site in HTTPX_BASED_SITES:
+            scraper = site.scraper_class(client)
+            htmls = await scraper.scrape(site.base_url, keywords)
+            if isinstance(htmls, str):
+                htmls = (htmls,)
+            for html in htmls:
+                parser = site.parser_class(html)
+                jobs.extend(parser.parse_jobs(keywords))
+    return jobs
 
 
 @app.get("/api/get-jobs")
