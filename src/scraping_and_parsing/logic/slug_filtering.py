5a3ed85e148c9c83738ed526761ba0d8e2010b08
Yehor Sencha <senyehor@gmail.com>
2025-02-04 18:17:04 +0200

    feat: make separate function for each site type

--- src/scraping_and_parsing/logic/slug_filtering.py
+++ src/scraping_and_parsing/logic/slug_filtering.py
@@ -1,7 +1,6 @@
-from functools import partial
 from typing import Iterable
 
-from src.scraping_and_parsing.sites import Playwright_SITES, SELENIUM_SITES, SiteBase
+from src.scraping_and_parsing.sites import HTTPX_SITES, Playwright_SITES, SELENIUM_SITES, SiteBase
 
 
 def _filter_sites_dict_by_slug(
@@ -10,6 +9,22 @@ def _filter_sites_dict_by_slug(
     return [sites[slug] for slug in site_slugs if slug in sites]
 
 
-filter_selenium_sites = partial(_filter_sites_dict_by_slug, sites=SELENIUM_SITES)
-filter_httpx_sites = partial(_filter_sites_dict_by_slug, sites=SELENIUM_SITES)
-filter_playwright_sites = partial(_filter_sites_dict_by_slug, sites=Playwright_SITES)
+def get_selenium_sites(
+        site_slugs: Iterable[str],
+        selenium_sites_dict: dict[str, type[SiteBase]] = SELENIUM_SITES
+) -> Iterable[type[SiteBase]]:
+    return _filter_sites_dict_by_slug(site_slugs, selenium_sites_dict)
+
+
+def get_httpx_sites(
+        site_slugs: Iterable[str],
+        httpx_sites_dict: dict[str, type[SiteBase]] = HTTPX_SITES
+) -> Iterable[type[SiteBase]]:
+    return _filter_sites_dict_by_slug(site_slugs, httpx_sites_dict)
+
+
+def get_playwright_sites(
+        site_slugs: Iterable[str],
+        playwright_sites_dict: dict[str, type[SiteBase]] = Playwright_SITES
+) -> Iterable[type[SiteBase]]:
+    return _filter_sites_dict_by_slug(site_slugs, playwright_sites_dict)
