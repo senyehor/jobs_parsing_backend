571a0411afabcec2dcf45783907669c699589a34
Yehor Sencha <senyehor@gmail.com>
2025-02-25 15:12:36 +0200

    ref: make return existing user if present

--- src/users/logic.py
+++ src/users/logic.py
@@ -4,15 +4,15 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from src.db.models import User
 
 
-async def register_user_if_not_already(
+async def register_or_get_user(
         db: AsyncSession, email: str, name: str,
         google_subject_id: int
-):
-    existing_user = await db.execute(select(User).where(User.email == email))
-    if existing_user.all():
-        # todo maybe exception
-        return None
+) -> User:
+    existing_user_query = await db.execute(select(User).where(User.email == email))
+    if existing_user := existing_user_query.scalars().first():
+        return existing_user
     new_user = User(email=email, name=name, google_subject_id=google_subject_id)
     db.add(new_user)
     await db.commit()
+    await db.refresh(new_user)
     return new_user
