c2dc11b1bdd16b99b51e4c4f4c081418813dd5dd
Yehor Sencha <senyehor@gmail.com>
2025-03-03 18:10:38 +0200

    ref: change typehints

--- src/scraping_and_parsing/logic/slug_filtering.py
+++ src/scraping_and_parsing/logic/slug_filtering.py
@@ -4,27 +4,27 @@ from src.scraping_and_parsing.sites import HTTPX_SITES, Playwright_SITES, SELENI
 
 
 def _filter_sites_dict_by_slug(
-        site_slugs: Iterable[str], sites: dict[str, type[SiteBase]]
-) -> Iterable[type[SiteBase]]:
+        site_slugs: Iterable[str], sites: dict[str, SiteBase]
+) -> Iterable[SiteBase]:
     return [sites[slug] for slug in site_slugs if slug in sites]
 
 
 def get_selenium_sites(
         site_slugs: Iterable[str],
-        selenium_sites_dict: dict[str, type[SiteBase]] = SELENIUM_SITES
-) -> Iterable[type[SiteBase]]:
+        selenium_sites_dict: dict[str, SiteBase] = SELENIUM_SITES
+) -> Iterable[SiteBase]:
     return _filter_sites_dict_by_slug(site_slugs, selenium_sites_dict)
 
 
 def get_httpx_sites(
         site_slugs: Iterable[str],
-        httpx_sites_dict: dict[str, type[SiteBase]] = HTTPX_SITES
-) -> Iterable[type[SiteBase]]:
+        httpx_sites_dict: dict[str, SiteBase] = HTTPX_SITES
+) -> Iterable[SiteBase]:
     return _filter_sites_dict_by_slug(site_slugs, httpx_sites_dict)
 
 
 def get_playwright_sites(
         site_slugs: Iterable[str],
-        playwright_sites_dict: dict[str, type[SiteBase]] = Playwright_SITES
-) -> Iterable[type[SiteBase]]:
+        playwright_sites_dict: dict[str, SiteBase] = Playwright_SITES
+) -> Iterable[SiteBase]:
     return _filter_sites_dict_by_slug(site_slugs, playwright_sites_dict)
